7e43746c2890fd394a8e633da3c89c1f629b57ea
Seokjun Ko <tjrwns1021@gmail.com>
2025-06-19 20:43:40 +0900

    refactor: 센서 이벤트 리스너 메서드 이름 on에서 listen으로 변경

--- src/app/features/eyeBlink/interface/ISensor.ts
+++ src/app/features/eyeBlink/interface/ISensor.ts
@@ -1,5 +1,5 @@
 export interface ISensor {
   getCurrentValue(): number;
-  on(event: string, listener: (value: number) => void): void;
+  listen(event: string, listener: (value: number) => void): void;
   off(event: string, listener: (value: number) => void): void;
 } 
