7faf3956f48973c645bf135cee64455257847256
Seokjun Ko <tjrwns1021@gmail.com>
2025-08-16 16:15:07 +0900

    범용성 높게 변경

--- src/app/interface/IMonitor.ts
+++ src/app/interface/IMonitor.ts
@@ -1,6 +1,7 @@
 export interface IMonitor {
   startMonitoring(): void;
   stopMonitoring(): void;
-  setWarning(eventName: string, callback: (data: any) => void): void;
-  deleteWarning(eventName: string): void;
+  recordEvent(at: Date): void; // 깜빡임, 얼굴 터치, 머리 터치 등 이벤트 발생 시 
+  snapshot(): {}; // 해당 모니터에서 유용한 정보를 받는 함수
+  needWarn(now: Date): boolean; // 해당 모니터에서 경고를 줄지 말지에 대한 기준으로 판단하는 함수
 }
