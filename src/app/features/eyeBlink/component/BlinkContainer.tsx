aa1f06e363be73bc22eb4717a0fec996518622cf
Seokjun Ko <tjrwns1021@gmail.com>
2025-08-29 15:33:22 +0900

    [Fix] pip 경고가 켜진 상태만 pip 창 켜짐

--- src/app/features/eyeBlink/component/BlinkContainer.tsx
+++ src/app/features/eyeBlink/component/BlinkContainer.tsx
@@ -60,7 +60,7 @@ export default function BlinkContainer() {
         <StatusIndicator isRunning={vm?.isRunning ?? false} />
       </div>
 
-      <CustomPiP isRunning={vm?.isRunning ?? false} />
+      { vm?.pipOn && <CustomPiP isRunning={vm?.isRunning ?? false} /> }
 
       {/* 하단 버튼 */}
       <div className="px-6 pb-6">
