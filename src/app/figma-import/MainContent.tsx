a4213ee0d41dd26854fb63a0e1829bfa5ed639d3
Seokjun Ko <tjrwns1021@gmail.com>
2025-09-09 18:00:39 +0900

    [ADD] Page를 그리기 위한 객체들 추가

--- src/app/figma-import/MainContent.tsx
+++ src/app/figma-import/MainContent.tsx
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Title from './Title';
 import InfoBadge from './InfoBadge';
 import { PlayButton } from '../components/PlayButton';
+import { BlinkViewController } from '../features/eyeBlink/controller/BlinkViewController';
+import { BlinkViewModel } from '../features/eyeBlink/presenter/BlinkPresenter';
+import { buildBlinkDIContainer } from '../di/BlinkDIContainer';
+import { DI_TOKENS } from '../di/DI_Token';
 
 const MainContent = () => {
+
+  const [vm, setVM] = useState<BlinkViewModel | null>(null);
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const [controller, setController] = useState<BlinkViewController | null>(null);
+
+
+  useEffect(() => {
+    if (!videoRef.current) return;
+    const di = buildBlinkDIContainer({
+      videoEl: videoRef.current,
+      setVM,
+    });
+
+    // 컨트롤러 얻기
+    const c = di.get<BlinkViewController>(DI_TOKENS.Blink.Controller);
+    // 뷰에서 컨트롤러 메서드 호출
+    
+    setController(c);
+    return () => {
+      c.monitorStop()
+      di.dispose();
+    };
+  }, []);
+
   return (
     <main className="flex flex-col items-center justify-center flex-grow">
       <div className="flex flex-col items-center">
