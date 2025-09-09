162814cc97df72290e04010370044f10bd2f46d3
Seokjun Ko <tjrwns1021@gmail.com>
2025-09-09 17:38:20 +0900

    [Fix] 디자인에 맞게 UI 수정

--- src/app/figma-import/Header.tsx
+++ src/app/figma-import/Header.tsx
@@ -4,7 +4,7 @@ import { SVGIcon } from '../components/SVGIcon';
 const Header = () => {
   return (
     <header className="flex items-center justify-between w-full h-14 bg-white px-14">
-      <div className="font-pretendard text-Body font-SemiBold text-BrandGreen">Blink</div>
+      <div className="font-pretendard text-Body font-semibold text-Primary-500">Blink</div>
       <div className="flex items-center space-x-4">
         <SVGIcon name={'Setting'} size={24} />
       </div>
