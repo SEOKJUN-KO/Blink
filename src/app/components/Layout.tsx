625cc185811bc7b2b1b11ba4b10218d2283cbde5
Seokjun Ko <tjrwns1021@gmail.com>
2025-09-14 19:39:57 +0900

    [Del] 필요없는 파일 제거

--- src/app/components/Layout.tsx
+++
@@ -1,9 +0,0 @@
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-8 bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100">
-      <main className="flex flex-col gap-8 p-8 bg-white dark:bg-gray-800 rounded-lg shadow-md w-full max-w-md">
-        {children}
-      </main>
-    </div>
-  );
-} 
