a52e2619cd0ea133b3692233a5f92c25ff57728f
Seokjun Ko <tjrwns1021@gmail.com>
2025-09-14 19:49:51 +0900

    [Fix] 호버시, 커서 변경 추가

--- src/app/components/SVGIcon.tsx
+++ src/app/components/SVGIcon.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 type IconProps = { name: string; width: number, height: number };
 
 export const SVGIcon: React.FC<IconProps> = ({ name, width, height }) => (
-  <div style={{ width: `${width}px`, height: `${height}px` }} className="flex items-center justify-center">
+  <div style={{ width: `${width}px`, height: `${height}px` }} className="flex items-center justify-center cursor-pointer">
     <Image src={`/${name}.svg`} alt={name} width={width} height={height} />
   </div>
 );
